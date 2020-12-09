fc40ccc9d919c3348cda587c93b98f0372ebed7a
Melaniem18 <73828280+Melaniem18@users.noreply.github.com>
2020-12-09 16:29:52 +0100

    exercice php

---
+++ USAL38/PHP/php_intro_exercices/01-hello.php
@@ -0,0 +1,18 @@
+<?php
+
+$helloWorld = "Hello World !";
+
+echo $helloWorld;
+
+
+
+
+function hello(string $name): string
+{
+    return "Hello $name";
+}
+echo hello("Mike");
+
+if ($name = ) {
+    return "Nobody";
+}
---
+++ USAL38/PHP/php_intro_exercices/02-numbers.php
@@ -0,0 +1,41 @@
+<?php
+
+$a = 5;
+$b = 3;
+
+$getSum = $a + $b;
+echo $getSum;
+
+echo "\n";
+
+$getSub = $a - $b;
+echo $getSub;
+
+echo "\n";
+
+$getSub = $b - $a;
+echo $getSub;
+
+echo "\n";
+
+$c = 5.6;
+$d = 3;
+
+$getMulti = $c * $d;
+echo $getMulti;
+
+echo "\n";
+
+$e = 5.6;
+$f = -3.7;
+
+$getMulti = $e * $f;
+echo $getMulti;
+
+echo "\n";
+
+$g = 20;
+$h = 3;
+
+$getDiv = $g / $h;
+echo $getDiv;
