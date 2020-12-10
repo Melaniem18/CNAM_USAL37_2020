39607ba8780992d8f8332455dacae834577b1ae3
Melaniem18 <73828280+Melaniem18@users.noreply.github.com>
2020-12-10 13:33:07 +0100

    correction tp agence

--- USAL37/TP Agence de voyage/Tp_AgenceVoyageClients.sql
+++ USAL37/TP Agence de voyage/Tp_AgenceVoyageClients.sql
@@ -16,6 +16,7 @@ client_email varchar(128) not null,
 client_phone varchar(16) not null,
 client_added date not null,
 client_password varchar(60) not null,
+com_code varchar(5) not null,
 foreign key (com_code) references sales(com_code)
 );
 
@@ -31,11 +32,11 @@ values
 insert into clients
 (client_id, client_lastname, client_firstname, client_email, client_phone, client_added,com_code)
 values
-(1), ("Rivière"), ("Charles"), ("rivierecharles@mail.com"), ("+336 76 24 71 92"), (2020-09-22), (22),
-(2),("Meunier"), ("Paulette"), ("meunierpaulette@mail.com"), ("+336 72 84 11 32"), (2019-11-19), (44),
-(3),("Boulanger"), ("Océane"), ("boulangeroceane@mail.com"), ("+336 24 34 71 22"), (2018-10-11), (55),
-(4),("Chevalier"), ("Pierre"), ("chevalierpierre@mail.com"), ("+336 64 84 41 62"), (2020-12-10), (11),
-(5),("Roger"), ("Lucas"), ("rogerlucas@mail.com"), ("+336 54 64 71 32"), (2020-02-11), (33);
+(1), ("Rivière"), ("Charles"), ("rivierecharles@mail.com"), ("+336 76 24 71 92"), (2020-09-22), ("Dupond Jean"),
+(2), ("Meunier"), ("Paulette"), ("meunierpaulette@mail.com"), ("+336 72 84 11 32"), (2019-11-19), ("Dubois Marie"),
+(3), ("Boulanger"), ("Océane"), ("boulangeroceane@mail.com"), ("+336 24 34 71 22"), (2018-10-11), (22), ("Martin Sophie"),
+(4), ("Chevalier"), ("Pierre"), ("chevalierpierre@mail.com"), ("+336 64 84 41 62"), (2020-12-10), (22), ("Martin Sophie"),
+(5), ("Roger"), ("Lucas"), ("rogerlucas@mail.com"), ("+336 54 64 71 32"), (2020-02-11), ("Durand Thomas");
 
 SELECT * FROM clients
 JOIN sales ON clients.com_name = sales.com_name
