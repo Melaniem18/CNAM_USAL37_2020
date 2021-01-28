b837bd8be9906132f33cec352bc2c65da7ed9bec
Melaniem18 <73828280+Melaniem18@users.noreply.github.com>
2021-01-28 16:39:04 +0100

    cours 28/01

---
+++ USAL37/TP_Bank/tp_bank2_proc.sql
@@ -0,0 +1,5 @@
+USE usal37_tp_bank;
+
+SELECT * FROM accounts;
+
+CALL transfer(3, 30, 31);
---
+++ USAL37/TP_Bank/tp_bank2_select.sql
@@ -0,0 +1,17 @@
+USE usal37_tp_bank;
+
+SELECT * FROM banks;
+
+SELECT * FROM accounts;
+
+SELECT account_balance FROM accounts WHERE account_id = 20;
+
+
+
+
+
+
+
+
+
+
