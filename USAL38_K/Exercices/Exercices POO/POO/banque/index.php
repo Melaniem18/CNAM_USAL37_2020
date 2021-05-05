aa45aa4a6f1496f2e274751df720be152ade3603
Melaniem18 <73828280+Melaniem18@users.noreply.github.com>
2021-05-05 12:07:04 +0200

    delete file

--- USAL38_K/Exercices/Exercices POO/POO/banque/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'CompteCourant.php';
-
-$account1 = new CompteCourant();
-
-echo $account1;
--- USAL38_K/Gestion_etudiant/verif_authentification.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['NIVEAU'])){
-header("location:index.php");
-exit;
-}
-?>
